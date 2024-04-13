37f83c1c103cae633a037883196e91d4c9a246c0
ArnaldWeslen05 <arnaldclash@gmail.com>
2024-04-13 19:49:05 -0300

    feat(front): Cria Input para fazer pergunta

--- front/src/componetes/Chat.js
+++ front/src/componetes/Chat.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { IoIosSend } from "react-icons/io";
+import './chat.css'
 
 const Corpo = () =>{
     return(
         <div>
            <section>
              <div className="corpo">
-                <div className="caixa"></div>
+                <div className="caixa">
+                <div className="pergunta">
+                    <input type="text" placeholder="Pergunte ao gestor..." required/>
+                    <button><IoIosSend /></button>
+                    
+                  </div>
+                </div>
              </div>
            </section>
 
