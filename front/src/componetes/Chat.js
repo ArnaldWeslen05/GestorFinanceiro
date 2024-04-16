af9ea3de29b4701c81827208cde1f12b6ae66bda
ArnaldWeslen05 <arnaldclash@gmail.com>
2024-04-16 01:22:49 -0300

    Style(front): Cor de fundo

--- front/src/componetes/Chat.js
+++ front/src/componetes/Chat.js
@@ -1,24 +1,26 @@
-import React from "react";
+
 import { IoIosSend } from "react-icons/io";
-import './chat.css'
+import './chat.css';
 
 const Corpo = () =>{
+
+
     return(
         <div>
-           <section>
-             <div className="corpo">
-                <div className="caixa">
-                <div className="pergunta">
-                  
-                    <input type="text" placeholder="Pergunte ao gestor..." required/>
-                    <button><IoIosSend /></button>
-                    
-                  </div>
+            <section>
+                <div className="corpo">
+                    <div className="caixa">
+                        <div className="pergunta">
+                            <input 
+                                type="text" 
+                                id="pergunta" 
+                                placeholder="Pergunte ao gestor..."
+                                 required  />
+                            <button><IoIosSend /></button>
+                        </div>
+                    </div>
                 </div>
-             </div>
-           </section>
-
-
+            </section>
         </div>
     )
 }
