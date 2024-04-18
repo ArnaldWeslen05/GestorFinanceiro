389fc9183468989520eacd2de128d6effc7d7892
Arnald Weslen <arnaldclash@gmail.com>
2024-04-18 20:52:26 -0300

    Style(front): Animação Menu

--- front/src/componetes/Cabecario.js
+++ front/src/componetes/Cabecario.js
@@ -1,16 +1,30 @@
 import React from "react"
 
 const Cabecario = () =>{
+  
+  function abrirMenu(){
+    const menu = document.getElementById('menu')
+    if(menu.style.display === 'none'){
+        menu.style.display = 'block'
+    }else{
+      menu.style.display = 'none'
+    }
+  }
     
     return(
         <div>
              <div className="cabeçario">
                <h1>Gestor Financeiro</h1>
              </div>
-             <div className="menu">
+             <div className="btn-menu"  onClick={abrirMenu}>
                   <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
                       <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
                     </svg>
+              </div>
+              <div id="menu" className="menu">
+                 <p>Investimentos</p>
+                 <p>Educação Financeira</p>
+                 <p>Notícias</p>
               </div>
             
         </div> 
