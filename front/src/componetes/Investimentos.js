b13c9c661c9fbe15a2d848bd3e5411540f740091
ArnaldWeslen05 <arnaldclash@gmail.com>
2024-04-19 20:47:35 -0300

    Style(front): Estilizando o componete Investimentos

--- front/src/componetes/Investimentos.js
+++ front/src/componetes/Investimentos.js
@@ -1,9 +1,31 @@
-import './investimentos.css'
+import { FaBitcoin } from "react-icons/fa";
+import { FaChartLine } from "react-icons/fa6";
+import './investimentos.css';
+
 const Investimentos = () =>{
    return(
     <div>
         <div className="container-investimentos">
-           <div className="caixa-investimentos"></div>
+           <div className="caixa-investimentos">
+            
+            <div className="box-investimentos">
+                <p>Rendimentos CDI </p> 
+                <p><FaChartLine /></p> 
+            </div>
+
+            <div className="box-investimentos">
+                <p>Criptos Moedas</p>
+                <p><FaBitcoin /></p>
+            </div>
+
+            <div className="box-investimentos">
+                <p>Outros Investimentos</p>
+            </div>
+
+            <div className="box-investimentos">
+                <p>Outros Investimentos</p>
+            </div>
+           </div>
         </div>
     </div>
    )
