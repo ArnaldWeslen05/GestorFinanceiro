4c054c7fbc3c2f01925d4b11f1b9aae8891ddcd1
ArnaldWeslen05 <arnaldclash@gmail.com>
2024-04-22 20:52:17 -0300

    Feat(front): Cria Interface CDI

--- front/src/componetes/Cdi.js
+++ front/src/componetes/Cdi.js
@@ -1,6 +1,6 @@
 
 import { IoCloseCircle } from "react-icons/io5";
-import './investimentos.css';
+import './Cdi.css';
 
 const Cdi = ({onClose}) =>{
    return(
@@ -8,22 +8,32 @@ const Cdi = ({onClose}) =>{
         <div className="container-investimentos">
         <button className="close" onClick={onClose}><IoCloseCircle /></button>
            <div className="caixa-investimentos">
-            <div className="box-investimentos">
-                <p>Rendimentos CDI </p> 
-            
-            </div>
+                <div className="caixa-cdi">
+                    <input type="checkbox" name="BancoCdi" id=""/>
+                    <p>Nubank</p>
+                </div>
 
-            <div className="box-investimentos">
-                <p>Criptos Moedas</p>
-            </div>
+                <div className="caixa-cdi">
+                   <input type="checkbox" name="BancoCdi" id="" />
+                   <p>PicPay</p>
+                </div>
 
-            <div className="box-investimentos">
-                <p>Fundos Imobíliario</p>
-            </div>
+                <div className="caixa-cdi">
+                    <input type="checkbox" name="BancoCdi" id="" />
+                    <p>Inter</p>
+                </div>
+                <div className="caixa-cdi">
+                  <input type="checkbox" name="BancoCdi" id="" />
+                  <p>Bradesco</p>
+                </div>
 
-            <div className="box-investimentos">
-                <p>Outros Investimentos</p>
-            </div>
+                <div className="resultado-cdi">
+                     <h1 className="tt"> Valor: </h1>
+                     <input type="number" name="" id="" />
+                    <h1 className="tt">Meses: </h1>
+                    <input type="number" name="" id="" />
+                </div>
+                <h1 className="res">Resultado </h1>
            </div>
         </div>
     </div>
