9a8a5a43e028c17656d8bf2b20afac2d922fd308
ArnaldWeslen05 <arnaldclash@gmail.com>
2024-04-25 16:33:05 -0300

    wip(front): entrega back com front , chat Gpt

--- front/src/componetes/Chat.js
+++ front/src/componetes/Chat.js
@@ -1,10 +1,19 @@
 
 import { IoIosSend } from "react-icons/io";
 import './chat.css';
+import axios from 'axios';
 
 const Corpo = () =>{
-
-  
+  function enviar(){
+    const valor = document.getElementById('pergunta').value
+    axios.post('http://localhost:4000/pergunte-ao-chatgpt', {prompt: valor})
+    .then(response => {
+        console.log(response);
+        document.getElementById("caixa").value = response.data.completion;
+    }).catch(erro => {
+        console.log(erro);
+    })
+  }
     return(
         <div>
             <section>
@@ -17,7 +26,7 @@ const Corpo = () =>{
                                 id="pergunta" 
                                 placeholder="Pergunte ao gestor..."
                                  required  />
-                            <button ><IoIosSend /></button>
+                            <button onClick={enviar}><IoIosSend /></button>
                         </div>
                     </div>
                 </div>
