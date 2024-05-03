e4fdcec4b7b4d4c23b2d55f3e64796648acc0a88
ArnaldWeslen05 <arnaldclash@gmail.com>
2024-05-03 18:49:22 -0300

    feat(back): envia dados de pergunta e resposta para o banco de dados

--- back/BancoDeDados/index.js
+++ back/BancoDeDados/index.js
@@ -9,14 +9,31 @@ const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password:'1234',
-    database: 'crud'
+    database: 'logs'
 })
-
 connection.connect(err => {
     if(err) {console.log('erro ao se conectar ao banco');
     return;
 }
-    console.log('conectado com sucesso');
+    console.log('conectado ao banco com sucesso');
+})
+
+app.use(express.json())
+app.use(cors())
+
+//criando rota 
+app.post('/dados', (req, res)=>{
+    const {pergunta, resposta} = req.body;                       
+    const INSERT_QUERY = `INSERT INTO log_table (pergunta, resposta) VALUES (?, ?)`;
+    connection.query(INSERT_QUERY, [pergunta, resposta], (err, result) => {
+        if(err){
+            console.log('Erro ao inserir dados:', err);
+          res.status(500).send('Error ao inserir dados:',)
+        return;
+     }
+     console.log('Dados Inseridos com sucesso!');
+     res.status(500).send('Dados inseridos com sucesso')
+    })
 })
 
 
