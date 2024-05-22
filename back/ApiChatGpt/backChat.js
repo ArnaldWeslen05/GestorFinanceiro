3777284a4ab779dd214cce819f63b97be07a468e
ArnaldWeslen05 <arnaldclash@gmail.com>
2024-05-22 19:20:15 -0300

    fix(back): bug resolvido

--- back/ApiChatGpt/backChat.js
+++ back/ApiChatGpt/backChat.js
@@ -1,27 +1,27 @@
-require("dotenv").config();
-const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
-const { OpenAI } = require("openai");
-const openai = new OpenAI(OPENAI_API_KEY);
+    require("dotenv").config();
+    const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+    const { OpenAI } = require("openai");
+    const openai = new OpenAI(OPENAI_API_KEY);
 
-const express = require("express");
-const cors = require("cors");
-const app = express();
-app.use(express.json());
-app.use(cors());
+    const express = require("express");
+    const cors = require("cors");
+    const app = express();
+    app.use(express.json());
+    app.use(cors());
 
-app.post("/pergunte-ao-chatgpt", async (req, res) => {
-    const { prompt } = req.body;
-    const model = "gpt-3.5-turbo";
-    const role = "user";
-    const max_tokens = 50;
-    const completion = await openai.chat.completions.create({
-        messages: [{ role: role, content: prompt }],
-        model: model,
-        max_tokens: max_tokens,
+    app.post("/pergunte-ao-chatgpt", async (req, res) => {
+        const { prompt } = req.body;
+        const model = "gpt-3.5-turbo";
+        const role = "user";
+        const max_tokens = 50;
+        const completion = await openai.chat.completions.create({
+            messages: [{ role: role, content: prompt }],
+            model: model,
+            max_tokens: max_tokens,
+        });
+        res.json({ completion: completion.choices[0].message.content });
     });
-    res.json({ completion: completion.choices[0].message.content });
-});
 
-app.listen(4000, () =>
-    console.log("ChatGPT_Backend em execução na porta 4000")
-);
+    app.listen(4000, () =>
+        console.log("ChatGPT_Backend em execução na porta 4000")
+    );
