ddd2a40ba4255e0e85379967c629f9c083f4d1c6
ArnaldWeslen05 <arnaldclash@gmail.com>
2024-05-24 03:32:09 -0300

    feat(front): cria tabela par transações

--- front/src/componetes/Financias.js
+++ front/src/componetes/Financias.js
@@ -3,43 +3,75 @@ import './financias.css';
 import axios from 'axios';
 import { IoCloseCircle } from "react-icons/io5";
 
-const Financias = ({onCloseFinancias}) =>{
-    const [entrada, setEntrada] = useState(0);
-    const [saida, setSaida] = useState(0);
+const Financias = ({ onCloseFinancias }) => {
+    const [entrada, setEntrada] = useState('');
+    const [saida, setSaida] = useState('');
+    const [transacao, setTransacao] = useState([]);
+
+    const handleSave = async () => {
+        if(entrada === ''){
+            setEntrada(0)
+        }
+        if(saida === ''){
+            setSaida(0)
+        }
+        const total = entrada - saida
+        try {
+            const response = await axios.post('http://localhost:5002/financias', { entrada, saida });
+            if (response.data.success) {
+                setTransacao([...transacao, { entrada, saida, total}]);
+                setEntrada('');
+                setSaida('');
+            } else {
+                alert('Erro ao salvar');
+            }
+        } catch (error) {
+            console.error('Erro ao salvar as finanças', error);
+            alert('Error em nosso servidor. Você foi desconectado');
+            window.location.reload()
+        }
+    };
+
     
 
-    const handleSave = async () =>{
-        try{
-           const response = await axios.post('http://localhost:5002/financias', {entrada, saida});
-           if(response.data.success){
-            alert('dados Atualizados com sucesso!')
-           }else{
-            alert('Erro ao salvar')
-           }
-        }catch(error){
-            console.error('erro ao salvar as financias', error);
-            alert('Erro ao Atualizar so dados. Erro de servidor. Tente mais tarde')
-        }
-    }
-    return(
+    return (
         <div>
             <div className="container-financias">
-                <button className="close" onClick={onCloseFinancias}><IoCloseCircle /> </button>
                 <div className="box-financias">
+                <button className="close" onClick={onCloseFinancias}><IoCloseCircle /></button>
+
                     <div className="caixa-financias">
-                       <label htmlFor="entrada">Entradas:</label>
-                        <input type="number" value={entrada} onChange={(e) => setEntrada(+e.target.value)}/>
-                        <label htmlFor="entrada" >Saidas:</label>
-                        <input type="number" value={saida} onChange={(e) => setSaida(+e.target.value)}/>
-                        {/* <label htmlFor="entrada">Saldo:</label>
-                        <input type="number" readOnly/> */}
+                        <label htmlFor="entrada">Entradas:</label>
+                        <input type="number" value={entrada} onChange={(e) => setEntrada(+e.target.value)} />
+                        <label htmlFor="saida">Saídas:</label>
+                        <input type="number" value={saida} onChange={(e) => setSaida(+e.target.value)} />
                         <button className='save-financias' onClick={handleSave}>Salvar</button>
                     </div>
-                   
+                    <div className="transacoes">
+                        <h2>Transações Recentes</h2>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Entrada</th>
+                                    <th>Saída</th>
+                                    <th>Total</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {transacao.map((transaction, index) => (
+                                    <tr key={index}>
+                                      <td>R${transaction.entrada}</td>
+                                      <td>R${transaction.saida}</td>
+                                      <td className={transaction.total >= 0 ? 'positive' : 'negative'}>R${transaction.total}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Financias
+export default Financias;
